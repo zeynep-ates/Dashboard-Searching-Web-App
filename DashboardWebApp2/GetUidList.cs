fce53c8ef4a2404e4f431b9d9b4d562bd2ee8087
zeynep-ates <zeynep.ates@ozu.edu.tr>
2025-07-25 14:14:56 +0300

    clean revised push

---
+++ DashboardWebApp2/GetUidList.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DashboardWebApp2
+{
+    public class GetUidList(IConfiguration configuration)
+    {
+        public List<string> UidList = new List<string> { configuration["Secrets:Uids"] };
+    }
+}
---
+++ DashboardWebApp2/Pages/Search.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Text.Encodings.Web;
+using ZeynepWsClient;
+
+namespace DashboardWebApp2.Pages
+{
+    public class SearchModel : PageModel
+    {
+        SearchingClass searchingClass = new SearchingClass();
+        public string searchResponseText { get; set; }  
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            /*
+            Service1Client zeynepWsClient = new Service1Client();
+            SearchIDRequest searchIDRequest = new SearchIDRequest();
+            searchIDRequest.telemetryID = "2";
+            SearchResult[] searchResultList = zeynepWsClient.SearchIDAsync(searchIDRequest).Result.SearchIDResult;
+            */
+            var choice = Request.Form["choice"];
+            
+            var userInput = Request.Form["userInput"];
+
+            searchResponseText = string.Empty;
+
+            if (choice.ToString() == "inputID")
+            {
+                searchResponseText = searchingClass.SearchingID(userInput);
+            }
+            else
+            {
+                searchResponseText = searchingClass.SearchingName(userInput);
+            }
+        }
+    }
+}
